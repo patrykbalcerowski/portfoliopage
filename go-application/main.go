8f546645c19121ae322450bff4a40d1fd2463fb3
patrykbalcerowski <77065346+patrykbalcerowski@users.noreply.github.com>
2026-06-02 13:29:47 +0200

    visual change

--- go-application/main.go
+++ go-application/main.go
@@ -209,7 +209,18 @@ const landingPageHTML = `
                 </p>
             </div>
         </section>
-
+        <section class="links-grid">
+            <a href="https://github.com/patrykbalcerowski/portfoliopage" class="btn btn-github" target="_blank">
+                <svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.462-1.11-1.462-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.831.092-.646.35-1.086.636-1.336-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.578 9.578 0 0112 6.836c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.025 2.747-1.025.546 1.379.203 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.161 22 16.416 22 12c0-5.523-4.477-10-10-10z"></path></svg>
+                GitHub Repo
+            </a>
+            <a href="https://argocd.balc-tech.pl" class="btn btn-argo" target="_blank">
+                🐙 ArgoCD Panel
+            </a>
+            <a href="https://grafana.balc-tech.pl" class="btn btn-grafana" target="_blank">
+                📊 Grafana Dashboards
+            </a>
+        </section>
         <section class="section-card">
             <h2 class="section-title">
                 <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"></path></svg>
@@ -250,18 +261,7 @@ const landingPageHTML = `
             </div>
         </section>
 
-        <section class="links-grid">
-            <a href="https://github.com/patrykbalcerowski/portfoliopage" class="btn btn-github" target="_blank">
-                <svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.462-1.11-1.462-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.831.092-.646.35-1.086.636-1.336-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.578 9.578 0 0112 6.836c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.025 2.747-1.025.546 1.379.203 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.161 22 16.416 22 12c0-5.523-4.477-10-10-10z"></path></svg>
-                GitHub Repo
-            </a>
-            <a href="https://argocd.balc-tech.pl" class="btn btn-argo" target="_blank">
-                🐙 ArgoCD Panel
-            </a>
-            <a href="https://grafana.balc-tech.pl" class="btn btn-grafana" target="_blank">
-                📊 Grafana Dashboards
-            </a>
-        </section>
+        
     </div>
 </body>
 </html>
